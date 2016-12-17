75e021af22ca95e56b8063f5098d3bccca2ef074
9shikixp <c0115334ef@edu.teu.ac.jp>
2016-12-18 00:29:43 +0900

    datalistを実装してサジェストできるようになった

--- createDatalist.py
+++ createDatalist.py
@@ -4,3 +4,6 @@ con = sqlite3.connect('test.sqlite3')
 cur = con.cursor()
 
 cur.execute("select * from t_poke")
+for row in cur:
+	pokename = row[1]
+	print("<option value=\"" + pokename + "\"></option>")
