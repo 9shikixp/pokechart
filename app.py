be5605033f8ac9e8a64faa1a3a51caa9b29e81d6
9shikixp <c0115334ef@edu.teu.ac.jp>
2016-12-18 16:30:58 +0900

    計算結果をhtmlに反映

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ def post_requwst():
 	print(pokemon[5]=="")
 	con = sqlite3.connect('test.sqlite3')
 	cur = con.cursor()
-	pokedata = ""
+	# pokedata = ""
 	cur.execute("select * from t_poke where name like ? or name like ? or name like ? or name like ? or name like ? or name like ?", (pokemon[0],pokemon[1],pokemon[2],pokemon[3],pokemon[4],pokemon[5]))
 	firstresult = cur.fetchall()
 	for row in firstresult:
-		pokedata += addstr(row) + "<br>\n"
+		# pokedata += addstr(row) + "<br>\n"
 		typelist += [row[2]]
 
 	atkresult, defresult = typechart.partySuggest(typelist)
@@ -43,6 +43,7 @@ def post_requwst():
 	# and not type glob'*[" + "".join(typelist) + "]*'
 	print("atkresult","".join(atkresult))
 	print("defresult","".join(defresult))
+	sugtype = "攻撃側のおすすめ:" + "".join(atkresult) + "<br>\n防御側のおすすめ:" + "".join(defresult) + "\n"
 	suggestResult = cur.fetchall()
 	print(len(suggestResult)==0)
 	if len(suggestResult)==0:
@@ -51,11 +52,15 @@ def post_requwst():
 		suggestResult = cur.fetchall()
 	print(suggestResult)
 
+	sugpoke = "<hr>\nおすすめのポケモン<br>\n"
+	for row in suggestResult:
+		sugpoke += row[1] + ":" + row[2] + "<br>\n"
 	
 	print("pokemon: ", pokemon)
 
+
 	cur.close()
-	return render_template('index.html', message="計算結果", pokemon=pokemon) + pokedata
+	return render_template('index.html', message="計算結果", pokemon=pokemon) + sugtype + sugpoke
 
 if __name__ == "__main__":
 	app.run(debug=True)
